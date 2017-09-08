39e411f7f007e03385e95cb131729331dbce2d04
Guilherme Rossato <guilherme@fastcompras.com>
2017-09-08 15:36:14 -0300

    Added Request Classes Base
    Classes to load all commits and releases from all repositories that are being shown.

---
+++ script/classes/ReleaseRequest.js
@@ -0,0 +1,24 @@
+class ReleaseRequest extends GitRequest {
+	constructor() {
+		super();
+		this.headers = [{
+			name: 'Accept',
+			value: 'application/vnd.github.v3.raw+json',
+		},{
+			name: 'Content-Type',
+			value: 'application/vnd.github.v3.raw+json',
+		}];
+	}
+	dispatch() {
+		if (this.user && this.repository) {
+			this.url = `${this.url}/repos/${this.user}/${this.repository}/releases/`;
+			if (this.commit) {
+				this.url += this.commit;
+			}
+			super();
+		} else {
+			console.warn("Couldn't dispatch commit request due to missing parameters");
+		}
+		return this;
+	}
+}
